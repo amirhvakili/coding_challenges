1c04d0466e100a3ff95c1035337045944c4d0dab
Amir <amirhosein7210@gmail.com>
2022-06-06 17:09:19 +0430

    committed

--- coding_challenge3.py
+++ coding_challenge3.py
@@ -1,22 +1,20 @@
-#Exercise!
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+# Exercise!
+# Display the image below where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
 picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
 ]
 
-i=0
+i = 0
 for item in picture:
-  for i in item:
-    if(i==0):
-      print(" ", end = "")
-    elif(i==1):
-      print("*" , end = "")
-      
-  print("\n")
-  
-      
+    for i in item:
+        if(i == 0):
+            print(" ", end="")
+        elif(i == 1):
+            print("*", end="")
+
+    print("\n")
