f9eb1ef09da322d625962426303b9d528fc0da9c
Amir <amirhosein7210@gmail.com>
2022-06-07 12:37:08 +0430

    committed

--- coding_challenge5.py
+++ coding_challenge5.py
@@ -24,18 +24,18 @@ checkDriverAge()
 # also make it so that the default age is set to 0 if no argument is given.
 
 
-def checkDriverAge(age=0):
+# def checkDriverAge(age=0):
 
-    if int(age) < 18:
-        print(
-            "Sorry, you are too young to drive this car. Powering           off"
-        )
-    elif int(age) > 18:
-        print("Powering On. Enjoy the ride!")
-    elif int(age) == 18:
-        print(
-            "Congratulations on your first year of driving. Enjoy       the ride!"
-        )
+#     if int(age) < 18:
+#         print(
+#             "Sorry, you are too young to drive this car. Powering           off"
+#         )
+#     elif int(age) > 18:
+#         print("Powering On. Enjoy the ride!")
+#     elif int(age) == 18:
+#         print(
+#             "Congratulations on your first year of driving. Enjoy       the ride!"
+#         )
 
 
-checkDriverAge25()
+# checkDriverAge25()
