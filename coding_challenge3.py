45ea0f7bf29c07281714bf93ea4fc30a157eaa20
Amir <amirhosein7210@gmail.com>
2022-06-07 10:54:04 +0430

    committed

--- coding_challenge3.py
+++ coding_challenge3.py
@@ -1,4 +1,3 @@
-# Exercise!
 # Display the image below where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
 picture = [
     [0, 0, 0, 1, 0, 0, 0],
@@ -9,11 +8,18 @@ picture = [
     [0, 0, 0, 1, 0, 0, 0]
 ]
 
-for item in picture:
-    for pixel in item:
-        if pixel:
-            print(" ", end="")
-        else:
-            print("*", end="")
 
-    print("")
+def show_tree():
+    for item in picture:
+        for pixel in item:
+            if not pixel:
+                print(" ", end="")
+            else:
+                print("*", end="")
+
+        print("")
+
+
+show_tree()
+show_tree()
+show_tree()
