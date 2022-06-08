7e6afb53d1b2a946100cff7b07ee669b661b0334
Amir <amirhosein7210@gmail.com>
2022-06-08 12:04:01 +0430

    committed

--- coding_challenge7.py
+++ coding_challenge7.py
@@ -1,6 +1,6 @@
 some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 
-# Getting all the duplicates of the list above using comprehension
+# Get all the duplicates of the above list using comprehension
 
 duplicates = list({char for char in some_list if some_list.count(char) > 1})
 
