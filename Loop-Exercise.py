92bfa070ac86a0898c6700bad0e9ada97139fa22
Amir <amirhosein7210@gmail.com>
2022-07-04 15:36:44 +0430

    committed

---
+++ Loop-Exercise.py
@@ -0,0 +1,19 @@
+# Exercise 1: Print First 10 natural
+# numbers using while loop
+
+# Expected output:
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+number = 1
+while number < 11:
+    print(number)
+    number += 1
