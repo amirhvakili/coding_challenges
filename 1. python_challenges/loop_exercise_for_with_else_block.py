b73b6534c3d8a263005113ce8ef0c8728d948c92
Amir <amirhosein7210@gmail.com>
2022-07-05 17:33:20 +0430

    committed

---
+++ 1. python_challenges/loop_exercise_for_with_else_block.py
@@ -0,0 +1,15 @@
+# Use else block to display a
+# message “Done” after successful
+# execution of for loop
+
+# Same as the if statement,
+# Python allows us to use an else block
+# along with for loop. for loop
+# can have the else block, which
+# will be executed when the loop
+# terminates normally.
+
+for i in range(5):
+    print(i)
+else:
+    print('Done!')
---
+++ 1. python_challenges/loop_exercise_list_reversed.py
@@ -0,0 +1,7 @@
+# Print list in reverse order
+# using a loop
+
+list1 = [10, 20, 30, 40, 50]
+
+for item in reversed(list1):
+    print(item)
