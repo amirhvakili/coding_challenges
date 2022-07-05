d0f8d950ae1c152c67dc93f8ffbb81175ad966b7
Amir <amirhosein7210@gmail.com>
2022-07-05 15:53:12 +0430

    committed

--- 1. python_challenges/loop_exercise_number_pattern2.py
+++ 1. python_challenges/loop_exercise_number_pattern2.py
@@ -13,13 +13,13 @@ while True:
         row = int(input('please insert the number of rows you want to have: '))
         print('Here is your beautiful pattern with the given rows:')
         column = row
-
         for i in range(1, row + 1):
             for j in range(column, 0, -1):
                 print(j, end=' ')
             print('')
             column -= 1
         break
+
     except:
         print('This is not a valid number. Please try again.')
         continue
