29df4ce00c7b7ccbf6a58b6e47637e3991017cab
Amir <amirhosein7210@gmail.com>
2022-07-05 15:28:29 +0430

    committed

--- 1. python_challenges/number_of_files.py
+++ 1. python_challenges/number_of_files.py
@@ -1,4 +1,7 @@
 import os
 
-print(len([file for file in os.listdir(
-    './first_30_challenges/') if file.endswith('.py')]))
+if (len([file for file in os.listdir(
+        './1. python_challenges/') if file.endswith('.py')])) >= 30:
+    print('Too many python files in a folder. Please create a new folder')
+else:
+    print('You can create more python files. No problem.')
