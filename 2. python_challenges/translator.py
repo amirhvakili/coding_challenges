2c78ef711521161e7c9196b38c74825c0329eed3
Amir <amirhosein7210@gmail.com>
2022-08-07 15:53:55 +0430

    committed

--- 2. python_challenges/translator.py
+++ 2. python_challenges/translator.py
@@ -5,7 +5,7 @@ translator = Translator()
 
 with open('./2. python_challenges/translation_text.txt', mode='r') as file:
     data = file.read()
-    translation = translator.translate(data)
+    translation = translator.translate(data, dest='es')
 
 with open('./2. python_challenges/translated.txt', mode='w') as file2:
     file2.write(translation.text)
