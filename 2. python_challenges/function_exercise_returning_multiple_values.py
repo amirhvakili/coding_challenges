44b9f8aeefaf1eb85ef0beedc149219e2813d4a6
Amir <amirhosein7210@gmail.com>
2022-08-09 18:05:23 +0430

    committed

---
+++ 2. python_challenges/function_exercise_returning_multiple_values.py
@@ -0,0 +1,28 @@
+# Return multiple values
+# from a function
+# Write a program to create
+# function calculation() such that
+# it can accept two variables and
+# calculate addition and subtraction.
+# Also, it must return both addition
+# and subtraction in a single return call.
+
+# Given:
+
+# def calculation(a, b):
+#     # Your Code
+
+# res = calculation(40, 10)
+# print(res)
+# Expected Output
+
+# 50, 30
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    return addition, subtraction
+
+
+result = calculation(10, 5)
+print(result)
